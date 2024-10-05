7b09b18dcb6bb26a86bae90bfe376e6627ba2bdb
Alex Gorbatchev <agorbatchev@squareup.com>
2024-10-04 17:27:58 -0700

    Working vrt

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM uber/web-base-image:16.15.0-buster
+FROM mcr.microsoft.com/playwright:v1.47.2-noble
 
 WORKDIR /baseui
 
@@ -7,12 +7,6 @@ WORKDIR /baseui
 COPY package.json yarn.lock /baseui/
 RUN yarn
 
-# Copy the current files to the docker image.
 COPY . .
-
-# For now, it does the trick - should we think about adding lerna, or something similar?
-RUN cd packages/baseweb-vscode-extension && yarn
-
-# Perform any build steps if you want binaries inside of the image
 RUN yarn build
 RUN yarn e2e:build
--- playwright-base.config.ts
+++ playwright-base.config.ts
@@ -10,24 +10,33 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
+  workers: '100%',
+  reporter: 'html',
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        isMobile: false,
+      },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
   retries: process.env.CI ? 2 : 0,
   use: {
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
+  },
+  expect: {
+    toMatchSnapshot: { maxDiffPixelRatio: 0.2 },
+    // toHaveScreenshot: { maxDiffPixelRatio: 0.2 },
   },
 };
 
