b6f6ff659c88d7c9484c313c9e02e3bdef623dfd
Alex Gorbatchev <agorbatchev@squareup.com>
2024-10-07 13:15:01 -0700

    agorbatchev: apply eslint --fix

--- src/stepper/types.ts
+++ src/stepper/types.ts
@@ -1,9 +1,9 @@
 /*
 Copyright (c) Uber Technologies, Inc.
+
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
-// import type * as React from 'react';
 import type { Override } from '../helpers/overrides';
 
 export type MobileHeaderOverrides = {
