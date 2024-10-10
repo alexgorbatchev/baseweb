6b835055f526e1822d9520179ed38eec613200ef
Alex Gorbatchev <agorbatchev@squareup.com>
2024-10-09 18:15:35 -0700

    Upgrade testing-library/react

--- src/popover/__tests__/stateful-container.test.tsx
+++ src/popover/__tests__/stateful-container.test.tsx
@@ -5,8 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 import * as React from 'react';
-import { act } from 'react-dom/test-utils';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 
 import { StatefulContainer, PLACEMENT, TRIGGER_TYPE } from '..';
 
