20e072edc4eab92d5fe1ce7fa1d426ea375b8d7d
Alex Gorbatchev <agorbatchev@squareup.com>
2024-10-16 15:24:19 -0700

    Revert missing tabs-motion.stories.tsx deleted by #5290

--- src/tabs-motion/__tests__/tabs-motion.scenario.tsx
+++ src/tabs-motion/__tests__/tabs-motion.scenario.tsx
@@ -5,7 +5,6 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-/* eslint-disable jsx-a11y/accessible-emoji */
 /* global window */
 
 import * as React from 'react';
